0c184a05c0c4a9a0f0f6b4114c3689ca56d0d351
yuripiffer <yuripiffer@hotmail.com>
2022-07-30 17:25:39 -0300

    Dynamo operations

--- infrastructure/dynamo/dynamo.go
+++ infrastructure/dynamo/dynamo.go
@@ -1,9 +1,14 @@
 package dynamo
 
 import (
+	"context"
 	"fmt"
 	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
+	"zmed_exam_manager/app_errors"
+	"zmed_exam_manager/model"
 )
 
 type provider struct {
@@ -18,4 +23,40 @@ func NewProvider(awsConfig aws.Config, table string) *provider {
 		client: client,
 		table:  table,
 	}
+}
+
+func (p *provider) Persist(ctx context.Context, data *model.Exam) app_errors.AppError {
+	dataMap, err := attributevalue.MarshalMap(data)
+	if err != nil {
+		return app_errors.NewInternalServerError("Error in persist Dynamodb", err)
+	}
+
+	params := &dynamodb.PutItemInput{
+		TableName: aws.String(p.table),
+		Item:      dataMap,
+	}
+
+	_, err = p.client.PutItem(ctx, params)
+	if err != nil {
+		return app_errors.NewInternalServerError("Error in persist Dynamodb", err)
+	}
+	return nil
+}
+
+func (p *provider) FindById(id string) (*model.Exam, app_errors.AppError) {
+	out, err := p.client.GetItem(context.TODO(), &dynamodb.GetItemInput{
+		TableName: aws.String(p.table),
+		Key: map[string]types.AttributeValue{
+			"id": &types.AttributeValueMemberS{Value: id},
+		},
+	})
+	if err != nil {
+		return nil, app_errors.NewInternalServerError("Error in persist Dynamodb", err)
+	}
+	var exam *model.Exam
+	err = attributevalue.UnmarshalMap(out.Item, exam)
+	if err != nil {
+		return nil, app_errors.NewInternalServerError("Error in Dynamodb unmarshal", err)
+	}
+	return exam, nil
 }
